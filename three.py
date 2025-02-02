55dc67f4aec70730df66221ad0f1f27809add540
Abid Hasan <91649692+Abidhasan7-IT@users.noreply.github.com>
2025-02-02 20:10:04 +0600

    Add files via upload
    number 3

---
+++ three.py
@@ -0,0 +1,12 @@
+# String Reverse: Write a Python function to reverse a given string
+# and return the reversed string.
+
+def reverse_string(s):
+    return s[::-1]
+
+
+Text= str(input("Enter text here: "))
+
+str = reverse_string(Text)
+print(str)  
+
