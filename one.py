e8bedfd444e4b4c5d01543fc03899d7f4dece897
unknown <abidhasanstudent20@gmail.com>
2025-02-02 19:37:44 +0600

    problem solve start

---
+++ one.py
@@ -0,0 +1,9 @@
+# Variable Swap: Write a Python program to swap the values of two variables without using a temporary variable.
+
+a= 50;
+
+b=30;
+
+a,b = b,a
+print("a value is :", a )
+print("b value is :", b )
