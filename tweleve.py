898bdec9712b2c5711cb8270bff687f51bc2e698
Abid Hasan <91649692+Abidhasan7-IT@users.noreply.github.com>
2025-02-03 20:14:49 +0600

    Add files via upload

---
+++ tweleve.py
@@ -0,0 +1,12 @@
+# Largest of Three Numbers: Write a Python program that takes three numbers as input and prints 
+# the largest among them.
+
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+largest = max(num1, num2, num3)
+
+print("The largest number is:", largest)
+
