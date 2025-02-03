2fc83500de3710497ba2a4ede91edd7d1343e8e3
Abid Hasan <91649692+Abidhasan7-IT@users.noreply.github.com>
2025-02-03 19:47:56 +0600

    Add files via upload

---
+++ nine.py
@@ -0,0 +1,9 @@
+# String Concatenation: Write a Python program that takes two strings 
+# as input and concatenates them into a single string without using the + operator.
+
+str1= str(input("Enter First String: "))
+str2= str(input("Enter Second String: "))
+
+add= f"{str1} {str2}"
+
+print("Concatenated String is: ", add)
