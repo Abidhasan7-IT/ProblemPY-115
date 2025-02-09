432cad09d45843a285ae276669107cf53d128abd
Abid Hasan <91649692+Abidhasan7-IT@users.noreply.github.com>
2025-02-09 10:12:29 +0600

    Add files via upload

---
+++ 41.py
@@ -0,0 +1,9 @@
+# List Element Count: Write a Python program to count the occurrences 
+# of a specific element in a given list.
+
+numbers = [23, 1, 45, 12, 7, 89, 34, 1, 7, 1]
+
+element = 1
+count = numbers.count(element)
+
+print(f"The element {element} appears {count} times in the list.")
