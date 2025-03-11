728c5f9d3fa5c1e5facfb7a96be3ad9662a26ab4
Abid Hasan <91649692+Abidhasan7-IT@users.noreply.github.com>
2025-03-11 23:27:23 +0600

    Add files via upload

---
+++ 43.py
@@ -0,0 +1,8 @@
+# List Comprehension: Given a list of integers, write a Python program to create a new list that 
+# contains the squares of the elements using list comprehension.
+
+numbers = [1, 2, 3, 4]
+
+squares = [x**2 for x in numbers]
+
+print(squares)
